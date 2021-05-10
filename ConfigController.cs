47f861e4ee589942dc200e769a164e9162b2c3f7
bastixx <bastixx669@gmail.com>
2021-05-10 18:42:17 +0200

    Added Filewatcher for seedfile (for flags), added system to remember window position, added config reader

---
+++ ConfigController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IniParser;
+using IniParser.Model;
+
+namespace OriWotWTracker
+{
+    static class ConfigController
+    {
+        
+        private static string configfile = "C:/moon/settings.ini";
+        private static readonly FileIniDataParser Parser = new FileIniDataParser();
+        public static IniData Config = Parser.ReadFile(configfile);
+
+        public static string GetConfig(string key, string defaultval = "")
+        {
+
+            if (Config["Tracker"][key] == null || Config["Tracker"][key] == "")
+            {
+                return defaultval;
+            }
+            else
+            {
+                return Config["Tracker"][key];
+
+            }
+        }
+
+        public static void SetConfig(string key, string value)
+        {
+            Config["Tracker"][key] = value;
+            Parser.WriteFile(configfile, Config);
+        }
+    }
+}
---
+++ SeedFileWatcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace OriWotWTracker
+{
+    class SeedFileWatcher
+    {
+        private static Controller controller;
+
+        public static void Run(Controller Controller)
+        {
+            controller = Controller;
+
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher filewatcher = new FileSystemWatcher();
+
+            filewatcher.Path = "C:\\moon";
+            filewatcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName;
+            filewatcher.Filter = ".currentseedpath";
+
+            filewatcher.Changed += ParseChanges;
+
+            filewatcher.EnableRaisingEvents = true;
+        }
+
+        public static void ParseChanges(object source, FileSystemEventArgs e)
+        {
+
+            // Small sleep to prevent the tracker trying to access the file before its ready.
+            Thread.Sleep(75);
+
+            try
+            {
+                string CurrentSeedPath = File.ReadAllText(e.FullPath);
+
+                controller.UpdateSeedName(CurrentSeedPath);
+            }
+            catch (IOException err)
+            {
+                if (err.HResult == -2147024864)
+                {
+                    return;
+                }
+
+                //Debug.WriteLine("Unable to parse the trackfile contents.");
+                return;
+            }
+            catch (Exception err)
+            {
+                //Debug.WriteLine("Unable to parse the trackfile contents.");
+                return;
+            }
+        }
+    }
+}
